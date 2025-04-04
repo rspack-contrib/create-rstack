37d2bab25d7d949dc5bc31d8a8452a55cc676921
holyfata <holyfata@outlook.com>
2025-04-05 02:56:49 +0800

    fix: vue+ts+setup create wrong eslint error

--- template-eslint/vue-ts/eslint.config.mjs
+++ template-eslint/vue-ts/eslint.config.mjs
@@ -2,11 +2,13 @@ import js from '@eslint/js';
 import vue from 'eslint-plugin-vue';
 import globals from 'globals';
 import ts from 'typescript-eslint';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
 
 export default [
   { languageOptions: { globals: globals.browser } },
   js.configs.recommended,
   ...ts.configs.recommended,
   ...vue.configs['flat/essential'],
+  ...defineConfigWithVueTs(vueTsConfigs.recommended),
   { ignores: ['dist/'] },
 ];
