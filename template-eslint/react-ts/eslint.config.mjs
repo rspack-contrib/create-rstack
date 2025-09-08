cb3e068b30bedd58418c424ea646fd31a962c9f8
wood3n <wangkka1@163.com>
2025-09-08 11:05:56 +0800

    chore(deps): update eslint version and replace tseslint.config with defineConfig

--- template-eslint/react-ts/eslint.config.mjs
+++ template-eslint/react-ts/eslint.config.mjs
@@ -1,11 +1,11 @@
 import js from '@eslint/js';
-import { globalIgnores } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config([
+export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
--- template-eslint/svelte-ts/eslint.config.mjs
+++ template-eslint/svelte-ts/eslint.config.mjs
@@ -3,7 +3,7 @@ import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import ts from 'typescript-eslint';
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
   ...ts.configs.recommended,
